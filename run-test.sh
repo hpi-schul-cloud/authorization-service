873a0f2cbdfb0e1789b06ad064b91860a3b502b2
blazejpass <blazej.szczepanowski@gca.pass-consulting.com>
2024-03-12 14:01:19 +0100

    Fix code according to suggestions

--- run-test.sh
+++ run-test.sh
@@ -1,5 +1,5 @@
-docker build -t postgres-pgtap:16.1 -f Dockerfile .
-docker run -d --rm --name pgtap-tests -v "./db/seed:/docker-entrypoint-initdb.d" -v "./test/test.sh:/test/test.sh" \
--e "POSTGRES_PASSWORD=example" -e "POSTGRES_USER=postgres"  -e "DATABASE=postgres"  -e "HOST=localhost"  -e "TESTS=/test/*.sql"  -e "PORT=5432" postgres-pgtap:16.1
+docker build -t postgres-pgtap:16.1 -f Dockerfile.test .
+docker run -d --rm --name pgtap-tests -v "./db/seed:/docker-entrypoint-initdb.d" -e "POSTGRES_PASSWORD=example" -e "POSTGRES_USER=postgres"\
+  -e "DATABASE=postgres"  -e "HOST=localhost"  -e "TESTS=/test/*.sql"  -e "PORT=5432" postgres-pgtap:16.1
 docker exec pgtap-tests /test/test.sh
 docker stop pgtap-tests
